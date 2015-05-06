42e069b15822db635302195fc37d0ac9d26f486b
class <class@CST_N417_05.cst.citytech.cuny.edu>
2015-05-06 16:24:18 -0400

    done!.

---
+++ StudentClass/src/student/StudentClass.java
@@ -0,0 +1,39 @@
+package student;
+
+public class StudentClass {
+
+    private String name, phoneNo, address;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
